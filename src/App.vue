8aad63698cf2b3c395bf826ac12d3a83764ccaa7
imad <immaad@gmail.com>
2018-11-27 02:21:23 +1100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <router-link to="/create">Create Customer</router-link> | 
+      <router-link to="/list">Show All</router-link>
+    </div>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import CreateCustomer from './components/CreateCustomer.vue'
+import ShowAll from './components/ShowAll.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/create', component: CreateCustomer },
+  { path: '/list', component: ShowAll }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  router
 }
 </script>
 
@@ -24,5 +44,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  color: darkgreen;
+  font-size: 1.2rem;
+  font-weight: bolder;
 }
 </style>
---
+++ src/components/ShowAll.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>Show All</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowAll',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
